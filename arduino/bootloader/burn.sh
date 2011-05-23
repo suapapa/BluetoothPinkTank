0a9bab2689920168f63ab0200fafa85e1019dc47
Homin Lee <ff4500@gmail.com>
2011-05-23 19:49:23 +0900

    bootloader: Fixed bootloader burn script

--- arduino/bootloader/burn.sh
+++ arduino/bootloader/burn.sh
@@ -11,11 +11,16 @@ LFUSE=0xD4
 #HFUSE=0xCA
 #LFUSE=0xDF
 
-AVRDUDE_OPTS="-cstk500 -pm8 -P/dev/ttyUSB0 -b115200"
-#AVRDUDE_OPTS="-cstk200 -pm8 -P/dev/parport1"
+AVRDUDE_OPTS="-C/usr/share/arduino/hardware/tools/avrdude.conf "
+AVRDUDE_OPTS=$AVRDUDE_OPTS"-patmega8 "
+AVRDUDE_OPTS=$AVRDUDE_OPTS"-cstk500 -P/dev/ttyUSB0 -b115200 "
+#AVRDUDE_OPTS=$AVRDUDE_OPTS"-cusbtiny "
+#AVRDUDE_OPTS=$AVRDUDE_OPTS"-cstk200 -P/dev/parport1 "
+#AVRDUDE_OPTS=$AVRDUDE_OPTS"-v -v -v -v "
 
-avrdude $AVRDUDE_OPTS -e -u -Ulock:w:0x3f:m -Uefuse:w:0x00:m -Uhfuse:w:$HFUSE:m -Ulfuse:w:$LFUSE:m
-avrdude $AVRDUDE_OPTS -Uflash:w:$BINARY -Ulock:w:0x0f:m
+
+avrdude $AVRDUDE_OPTS -e -u -Ulock:w:0x3f:m -Uhfuse:w:$HFUSE:m -Ulfuse:w:$LFUSE:m
+avrdude $AVRDUDE_OPTS -Uflash:w:$BINARY:i -Ulock:w:0x0f:m
 
 # FYI: http://www.arduino.cc/playground/Learning/Atmega83-3V
 # FYI : http://www.avrfreaks.net/index.php?name=PNphpBB2&file=printview&t=96696&start=0
